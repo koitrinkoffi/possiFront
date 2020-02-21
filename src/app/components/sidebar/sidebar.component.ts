1a45b830351bff494d25e6dbab13f770f77df464
Koitrin KOFFI <koitrinstephanedesire@gmail.com>
2020-02-21 11:29:17 +0100

    feat: ajout du lien vers les plannings d'un prof

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -9,11 +9,11 @@ declare interface RouteInfo {
 }
 export const ROUTES: RouteInfo[] = [
     { path: '/home', title: 'Accueil',  icon: 'home', class: '' },
-    { path: '#', title: 'Mes plannings',  icon: 'calendar_today', class: '' },
+    { path: '/private/planning', title: 'Mes plannings',  icon: 'calendar_today', class: '' },
     { path: '#', title: 'Plannings assignés',  icon: 'assignment', class: '' },
     { path: '/create/planning', title: 'Nouveau planning',  icon: 'note_add', class: '' },
     { path: '/student/register', title: 'Informations personnelles',  icon: 'account_circle', class: '' },
-    // { path: '/maps', title: 'Maps',  icon: 'location_on', class: '' },
+    // { path: '/private/planning', title: 'Maps',  icon: 'location_on', class: '' },
     // { path: '/notifications', title: 'Notifications',  icon: 'notifications', class: '' },
     // { path: '/upgrade', title: 'Upgrade to PRO',  icon: 'unarchive', class: 'active-pro' },
 ];
--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -3,10 +3,12 @@ import {HomeComponent} from '../../components/home/home.component';
 import {UnavailabilityComponent} from '../../components/unavailability/unavailability.component';
 import {CreatePlanningComponent} from '../../components/create-planning/create-planning.component';
 import {StudentRegisterComponent} from '../../components/student-register/student-register.component';
+import {PrivatePlanningComponent} from '../../components/private-planning/private-planning.component';
 
 export const AdminLayoutRoutes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: ':planningName/unavailability', component: UnavailabilityComponent },
   { path: 'create/planning', component: CreatePlanningComponent },
-  { path: 'student/register', component: StudentRegisterComponent}
+  { path: 'student/register', component: StudentRegisterComponent},
+  { path: 'private/planning', component: PrivatePlanningComponent}
 ];
