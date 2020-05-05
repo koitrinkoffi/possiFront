b774b9534b65b0559f9d6a1de8f04ba2ffd05f3f
Koitrin KOFFI <koitrinstephanedesire@gmail.com>
2020-05-06 01:13:21 +0200

    fix: file input config -prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  app_url: '148.60.11.215',
+  app_url: 'https://possiv2.istic.univ-rennes1.fr',
   cas_url: 'https://sso-cas.univ-rennes1.fr',
   cas_validate_url: 'https://sso-cas.univ-rennes1.fr/serviceValidate'
 
